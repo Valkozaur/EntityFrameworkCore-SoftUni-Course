2be8f00c69f3eac6856af0c6f95da1ba9e29ba10
Valentin Nikolov <val.nikolov03@gmail.com>
2020-07-18 15:09:51 +0200

    First ASP.NET CORE Skeleton Finished

---
+++ Auto-Mapping/FastFood.Core/Controllers/OrdersController.cs
@@ -0,0 +1,63 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Orders;
+
+    public class OrdersController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public OrdersController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            var viewOrder = new CreateOrderViewModel
+            {
+                Items = this.context.Items.Select(x => new { x.Id, x.Name }).ToDictionary(k => k.Id, v => v.Name),
+                Employees = this.context.Employees.Select(x => new { x.Id, x.Name }).ToDictionary(k => k.Id, v => v.Name),
+            };
+
+            return this.View(viewOrder);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateOrderInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Error", "Home");
+            }
+
+            var order = mapper.Map<Order>(model);
+            var orderItem = this.mapper.Map<OrderItem>(model);
+
+            orderItem.Order = order;
+
+            this.context.Orders.Add(order);
+            this.context.OrderItems.Add(orderItem);
+            this.context.SaveChanges();
+
+            return RedirectToAction("All", "Orders");
+        }
+
+        public IActionResult All()
+        {
+            var order = context.Orders
+                .ProjectTo<OrderAllViewModel>(this.mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(order);
+        }
+    }
+}
